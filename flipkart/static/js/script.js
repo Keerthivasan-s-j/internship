54c4d2cc8a27c2c1f040fb477181e8cfe9a98049
Keerthivasan-s-j <jagankeerthisri@gmail.com>
2024-07-22 10:55:04 +0530

    added side navbar functionality

---
+++ flipkart/static/js/script.js
@@ -0,0 +1,9 @@
+var side_nav = document.getElementById("side-nav-cnt");
+
+function open_nav(){
+    side_nav.style.display = "block";
+}
+
+function close_nav(){
+    side_nav.style.display = "none";
+}
